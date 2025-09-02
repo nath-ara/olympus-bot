a52b5ac42f7c320ea1afcd71c112058e9d42d653
nath-ara <nathanlebg147@gmail.com>
2025-09-02 18:02:54 +1100

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,9 @@
 import os
 
 TOKEN = os.environ.get("TOKEN")
-NAME = "Olympus"
-server = "https://discord.com/invite/odx"
+NAME = "Kyra✨"
+server = "https://discord.gg/X9NDdMf3xf"
 ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
-OWNER_IDS = [213347081799073793, 1070619070468214824]
-BotName = "Olympus"
-serverLink = "https://discord.com/invite/odx"
+OWNER_IDS = [1341478551764860958, 1341478551764860958]
+BotName = "Kyra✨"
+serverLink = "https://discord.gg/X9NDdMf3xf"
