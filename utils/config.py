487e608b3c7f06c5016321344067a8fdfefb2d41
nath-ara <nathanlebg147@gmail.com>
2025-09-08 17:30:45 +1100

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -4,6 +4,6 @@ TOKEN = os.environ.get("TOKEN")
 NAME = "Kyra✨"
 server = "https://discord.gg/X9NDdMf3xf"
 ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
-OWNER_IDS = [1341478551764860958, 1341478551764860958]
+OWNER_IDS = [1341478551764860958, 1179587826669592587, 1204961543528382467]
 BotName = "Kyra✨"
 serverLink = "https://discord.gg/X9NDdMf3xf"
